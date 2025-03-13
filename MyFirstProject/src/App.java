6e030afe662d21e3fe87411fe29ee0390f6f4f2b
Lyyean Wan2006 <lyyeanwan@gmail.com>
2025-03-13 09:15:52 +0700

    update file App.java

--- MyFirstProject/src/App.java
+++ MyFirstProject/src/App.java
@@ -1,5 +1,31 @@
+// import java.util.Arrays;
+
+// import java.util.Date;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // byte age = 30;
+        // int viewCount = 3_233_222;
+        // float price = 10.99F;
+        // char A = 'B';
+        // Date bb = new Date();
+        // System.out.println(bb);
+
+        // String message = "c:\\Wimdow\\...";
+        // System.out.println(message);
+
+        // array
+        // int[] numbers = new int[5];
+        // numbers[0]=1;
+        // numbers[1]=2;
+        // int[] numbers = {2,5,3,4,0,1};
+        // System.out.println(numbers.length);
+
+        // System.out.println(Arrays.toString(numbers));
+
+        int result = 10 -4;
+        result++;
+        System.out.println(result);
     }
 }
