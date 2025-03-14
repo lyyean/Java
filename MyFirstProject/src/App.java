7fd67ba78d53da7b7648ead592ea0bf0209e82ae
Lyyean Wan2006 <lyyeanwan@gmail.com>
2025-03-14 09:41:42 +0700

    update file App.java 01

--- MyFirstProject/src/App.java
+++ MyFirstProject/src/App.java
@@ -1,3 +1,9 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+// import java.text.NumberFormat;j
+// import java.util.Scanner;
+
 // import java.util.Arrays;
 
 // import java.util.Date;
@@ -24,8 +30,50 @@ public class App {
 
         // System.out.println(Arrays.toString(numbers));
 
-        int result = 10 -4;
-        result++;
-        System.out.println(result);
+        // int result = 10 -4;
+        // result++;
+        // System.out.println(result);
+
+        // Implicit casting
+        // byte > short > int > long > float > double
+        // String x = "1";
+        // int y =  Integer.parseInt(x) +2 ;
+        // System.out.println(y);
+
+        // Math Methon
+        // int result = (int) Math.round(Math.random()*100);
+        // int result = (int) (Math.random()*100);
+        // System.out.println(result);
+
+        // Number  Format
+        // String result = NumberFormat.getCurrencyInstance().format(1223323);
+        // System.out.println(result);
+
+        // Read Input
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Name: ");
+        // String name = scanner.nextLine().trim();
+        // System.out.println("You are "+name);
+
+        // double result =Math.pow(1, 3);
+        // System.out.println(result);
+
+        // Mortgage Calculator
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.print("Rate:");
+        float rate = scanner.nextFloat();
+        float Monthly_Interest = rate /100 /12;
+        System.out.print("Period (Years): ");
+        byte year = scanner.nextByte();
+        int Month_Of_Payment = year *12;
+
+        double mortgage = principal 
+        * (Monthly_Interest*Math.pow(1+Monthly_Interest, Month_Of_Payment))
+        /(Math.pow(1+Monthly_Interest, Month_Of_Payment)-1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println(mortgageFormatted);
     }
 }
